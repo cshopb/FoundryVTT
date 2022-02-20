b705ca782996792f188e9256c5e31b285d959fa1
Zsolt <cshopb@gmail.com>
2022-02-20 17:05:22 +0000

    - add the site to generate a name

--- Ironsworn/DelveLocation.js
+++ Ironsworn/DelveLocation.js
@@ -1,21 +1,99 @@
 // CONFIG.debug.hooks = true;
 
-async function rollOnTable(tableName) {
-    const { results } = await game
+/**
+ * Generates a HTML select box.
+ *
+ * @param {String} label 
+ * @param {String[]} options 
+ * @param {String} id 
+ *
+ * @returns String
+ */
+function selectBox(label, options, id) {
+    let optionsHtml = '';
+
+    _.forEach(
+        options,
+        (option) => {
+            optionsHtml += `<option value="${option}">${option}</option>`;
+        }
+    );
+
+
+    return `
+    <label for="${id}">${label}</label>
+    <select name="${id}" id="${id}">
+        ${optionsHtml}
+    </select>
+    `;
+}
+
+/**
+ * Returns a RollTable with the requested name.
+ *
+ * @param {String} tableName 
+ *
+ * @returns RollTable
+ */
+function findTable(tableName) {
+    return game
         .tables
-        .find(t => t.name === tableName)
+        .find(t => t.name === tableName);
+}
+
+/**
+ * Rolls on the table and returns the result.
+ *
+ * @param {String} tableName 
+ *
+ * @returns TableResult
+ */
+async function rollOnTable(tableName) {
+    const rollTable = findTable(tableName);
+
+    const { results } = await rollTable
         .roll();
 
     return results[0];
 }
 
+/**
+ * Returns a specific TableResult based on its text value.
+ *
+ * @param {String} tableName
+ * @param {String} tableResultText
+ *
+ * @returns TableResult
+ */
+function getSpecificTableResult(tableName, tableResultText) {
+    const rollTable = findTable(tableName);
+
+    return rollTable
+        .results
+        .find(r => r.data.text = tableResultText);
+}
+
+/**
+ * Returns all the options of a RollTable as an array of strings.
+ *
+ * @param {String} tableName
+ *
+ * @returns String[]
+ */
+function getTableResultTexts(tableName) {
+    return findTable(tableNames.domain)
+        .data
+        .results
+        .map(r => r.data.text);
+}
+
+/**
+ * Prints the message to chat.
+ *
+ * @param {String} message 
+ */
 function printMessage(message) {
     message = `<h2>Delve Location</h2><p>${message}</p>`;
-
-    const gm = game
-        .users
-        .filter(u => u.isGM)
-        .map(u => u._id);
 
     const chatData = {
         user: game.user._id,
@@ -29,18 +107,37 @@ function printMessage(message) {
     );
 }
 
+/**
+ * Generates the image HTML tag.
+ *
+ * @param {TableResult} data 
+ *
+ * @returns String
+ */
 function image(data) {
     const side = '40px';
 
     return `<img src="${data.img}" alt="${data.text}" width="${side}" height="${side}">`
 }
 
+/**
+ * Prints the Actor to chat with the image that the actor has.
+ *
+ * @param {Actor} actor 
+ */
 function printLocation(actor) {
     const img = image(actor);
 
     printMessage(img + ' ' + actor.link);
 }
 
+/**
+ * Retutns the date of the item with the requested name.
+ *
+ * @param {String} name Name of the item.
+ *
+ * @returns Object
+ */
 function itemData(name) {
     return game
         .items
@@ -48,13 +145,58 @@ function itemData(name) {
         .data;
 }
 
-async function generateLocation() {
-    const theme = await rollOnTable('Delve Theme');
-    const domain = await rollOnTable('Delve Domain');
+/**
+ * Generate a Site Name.
+ *
+ * @param {String} domainName
+ *
+ * @returns String
+ */
+async function generateSiteName(domainName) {
+    const { data: format } = await rollOnTable(tableNames.siteNameFormat);
+
+    let name = format.text;
+
+    const tables = name.match(/\[[A-Z]+[a-z]+\]/g);
+
+    for (const table of tables) {
+        let tableName = table.match(/[A-Z]+[a-z]+/g)[0];
+
+        if (tableName === 'Place') {
+            tableName = 'Place-' + domainName;
+        }
+
+        const { data: value } = await rollOnTable(tableNames.siteNameDefault + tableName);
+
+        name = name.replace(
+            table,
+            value.text
+        );
+    }
+
+    return name;
+}
+
+/**
+ * Generates a Delve Site.
+ *
+ * @param {String} domainName
+ */
+async function generateSite(domainName) {
+    let domain = {};
+
+    if (domainName === 'Random') {
+        domain = await rollOnTable(tableNames.domain);
+    } else {
+        domain = getSpecificTableResult(tableNames.domain, domainName);
+    }
+
+    const theme = await rollOnTable(tableNames.theme);
+    const name = await generateSiteName(domain.data.text);
 
     const actor = await Actor.create(
         {
-            'name': 'Site',
+            name,
             'type': 'site',
             'img': domain.icon,
             'items': [
@@ -67,4 +209,38 @@ async function generateLocation() {
     printLocation(actor);
 }
 
-generateLocation();
+const tableNames = {
+    siteNameFormat: 'Site Name: Format',
+    siteNameDefault: 'Site Name: ',
+    domain: 'Delve Domain',
+    theme: 'Delve Theme',
+}
+
+const gm = game
+    .users
+    .filter(u => u.isGM)
+    .map(u => u._id);
+
+new Dialog({
+    title: 'Create Roll Table',
+    content: selectBox(
+        'Select Domain',
+        [
+            'Random',
+            ...getTableResultTexts(tableNames.domain)
+        ],
+        'domain'
+    ),
+    buttons: {
+        yes: {
+            icon: '<i class="fas fa-check"></i>',
+            label: 'Apply Changes',
+            callback: () => {
+                const tableName = document.querySelector('#domain').value;
+
+                generateSite(tableName);
+            }
+        }
+    },
+    defaultYes: false
+}).render(true);
